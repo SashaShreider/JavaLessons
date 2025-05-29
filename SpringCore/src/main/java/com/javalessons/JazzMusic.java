5f38b7b7eca23f4be458d0e6535bf30106829a26
WaveWalker <90608911+SashaShreider@users.noreply.github.com>
2025-05-29 19:12:11 +0300

    fix(SpringCore): init и destroy методы в applicationContext.xml

--- SpringCore/src/main/java/com/javalessons/JazzMusic.java
+++ SpringCore/src/main/java/com/javalessons/JazzMusic.java
@@ -1,6 +1,12 @@
 package com.javalessons;
 
 public class JazzMusic implements Music{
+    void init(){
+        System.out.println("JazzMusic init");
+    }
+    void destroy(){
+        System.out.println("JazzMusic destroy");
+    }
     @Override
     public String getSong(){
         return "Jazz Music Song";
