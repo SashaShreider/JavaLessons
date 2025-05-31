3172134e9ed59162fd229ae0c8dafcd217295c87
WaveWalker <90608911+SashaShreider@users.noreply.github.com>
2025-05-31 04:31:17 +0300

    feat(SpringMVC): работа с классом Model

--- SpringMVC/src/main/java/com/javalessons/springmvc/controllers/GentleConroller.java
+++ SpringMVC/src/main/java/com/javalessons/springmvc/controllers/GentleConroller.java
@@ -1,25 +1,18 @@
 package com.javalessons.springmvc.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GentleConroller {
-
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-        System.out.println("Hello " + name + " " + surname);
-        return "gentle/hello";
-    }
-
-    public String helloPage2(@RequestParam(value = "name", required = false) String name,
-                             @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello " + name + " " + surname);
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+//        System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "gentle/hello";
     }
 
