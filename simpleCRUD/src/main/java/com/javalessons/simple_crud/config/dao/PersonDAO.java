72b31e529c1c2553a4d60cf2d1fc649135548aa3
WaveWalker <90608911+SashaShreider@users.noreply.github.com>
2025-06-02 20:00:54 +0300

    feat(SimpleCRUD): добавлена база клиентов, возможность просматривать всех людей и отдельно человека по индексу

---
+++ simpleCRUD/src/main/java/com/javalessons/simple_crud/config/dao/PersonDAO.java
@@ -0,0 +1,31 @@
+package com.javalessons.simple_crud.config.dao;
+
+import com.javalessons.simple_crud.config.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Jack"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Sally"));
+
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        }
+}
