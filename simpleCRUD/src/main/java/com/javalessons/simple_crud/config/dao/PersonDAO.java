d9532b3df83a481b28a95f7cfae7f0b829750b44
WaveWalker <90608911+SashaShreider@users.noreply.github.com>
2025-06-04 19:18:08 +0300

    feat(SimpleCRUD\dao): добавлены все CRUD операции с использованием PreparedStatement

--- simpleCRUD/src/main/java/com/javalessons/simple_crud/config/dao/PersonDAO.java
+++ simpleCRUD/src/main/java/com/javalessons/simple_crud/config/dao/PersonDAO.java
@@ -9,13 +9,11 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-
-    private static final String URL = "jdbc:postgresql://localhost:5432/users_db";
     public static final String USERNAME = "postgres";
     public static final String PASSWORD = "postgres";
-
+    private static final String URL = "jdbc:postgresql://localhost:5432/users_db";
     private static final Connection connection;
+    private static int PEOPLE_COUNT;
 
     static {
         try {
@@ -57,35 +55,74 @@ public class PersonDAO {
     }
 
     public Person show(int id) {
-//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-        return null;
+        Person person;
+
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("SELECT* FROM Person WHERE id=?");
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+
+            person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setAge(resultSet.getInt("age"));
+            person.setEmail(resultSet.getString("email"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return person;
     }
 
     public void save(Person person) {
-//        person.setId(++PEOPLE_COUNT);
-//        people.add(person);
-
         try {
-            Statement statement = connection.createStatement();
-            String query = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() +
-                    "'," + person.getAge() + ",'" + person.getEmail() + "')";
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO Person (id, name, age, email) VALUES (?, ?, ?, ?)"
+            );
 
-            statement.executeUpdate(query);
+            preparedStatement.setInt(++PEOPLE_COUNT, person.getId());
+            preparedStatement.setString(2, person.getName());
+            preparedStatement.setInt(3, person.getAge());
+            preparedStatement.setString(4, person.getEmail());
+
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void update(int id, Person person) {
-//        Person personToUpdate = show(id);
+    public void update(int id, Person updatedPerson) {
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id=?");
 
-//        personToUpdate.setName(person.getName());
-//        personToUpdate.setEmail(person.getEmail());
-//        personToUpdate.setAge(person.getAge());
+            preparedStatement.setString(1, updatedPerson.getName());
+            preparedStatement.setInt(2, updatedPerson.getAge());
+            preparedStatement.setString(3, updatedPerson.getEmail());
+            preparedStatement.setInt(4, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void delete(int id) {
-//        people.removeIf(person -> person.getId() == id);
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM Person WHERE id=?");
+
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
